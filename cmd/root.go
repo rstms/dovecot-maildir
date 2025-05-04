29d04b2a30a1267f27877ba7c51106cf5fd0faae
Matt Krueger <mkrueger@rstms.net>
2025-05-04 03:12:19 -0600

    add bumpversion config add version command add root.go update to bumpversion config

--- cmd/root.go
+++ cmd/root.go
@@ -40,6 +40,8 @@ import (
 
 var cfgFile string
 
+const Version = "0.0.2"
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dovecot-maildir",
