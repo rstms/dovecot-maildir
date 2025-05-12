8d514d2010e5c084123f888eb02f0970a7ab2572
Matt Krueger <mkrueger@rstms.net>
2025-05-12 12:53:37 -0600

    Bump version: 1.0.0 → 1.0.1

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ import (
 
 var cfgFile string
 
-const Version = "1.0.0"
+const Version = "1.0.1"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
